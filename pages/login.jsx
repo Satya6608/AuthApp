7fb7c06a0ad5e696f524043e002c79e86675d2d6
Satya prakash <87296687+Satya6608@users.noreply.github.com>
2023-11-27 08:19:53 +0530

    some change

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,10 +3,11 @@ import googlepik from '../assets/images/google-icon.svg'
 import applepik from '../assets/images/apple-icon.svg'
 
 import { signIn } from "next-auth/react"
+import { Provider } from 'next-auth/client'
 
 export default function Login() {
   function handleSignInGoogle(){
-    signIn('google',{callbackUrl:process.env.NEXTAUTH_URL || "http://localhost:3000"})
+    signIn(Provider.google)
   }
   function handleSignInApple(){
     signIn('apple',{callbackUrl:process.env.NEXTAUTH_URL || "http://localhost:3000"})
