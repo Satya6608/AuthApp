60e1bb59b05af234390347857b989c59540fbae9
satya <satyaprakash6608@gmail.com>
2024-02-29 22:29:51 +0530

    env setting

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import googlepik from '../assets/images/google-icon.svg'
-import applepik from '../assets/images/apple-icon.svg'
+import Image from "next/image";
+import googlepik from "../assets/images/google-icon.svg";
+import applepik from "../assets/images/apple-icon.svg";
 
-import { signIn } from "next-auth/react"
+import { signIn } from "next-auth/react";
 
 export default function Login() {
-  function handleSignInGoogle(){
-    signIn('google',{callbackUrl:process.env.NEXTAUTH_URL || "http://localhost:3000"})
+  function handleSignInGoogle() {
+    signIn("google", { callbackUrl: process.env.NEXTAUTH_URL });
   }
-  function handleSignInApple(){
-    signIn('apple',{callbackUrl:process.env.NEXTAUTH_URL || "http://localhost:3000"})
+  function handleSignInApple() {
+    signIn("apple", { callbackUrl: process.env.NEXTAUTH_URL });
   }
   return (
     <div className="flex h-[100vh]">
@@ -22,27 +22,31 @@ export default function Login() {
           <h2 className="text-[16px] mb-[15px]">Sign in to your account</h2>
           <div className="flex gap-[10px] justify-between">
             <button className="sign-icon" onClick={handleSignInGoogle}>
-              <Image src={googlepik} width={16}
-                height={16} alt="Google" />
+              <Image src={googlepik} width={16} height={16} alt="Google" />
               <span>Sign in with Google</span>
             </button>
             <button className="sign-icon" onClick={handleSignInApple}>
-              <Image src={applepik} width={16}
-                height={16} alt="Google" />
+              <Image src={applepik} width={16} height={16} alt="Google" />
               <span>Sign in with Apple</span>
             </button>
           </div>
-          <div className='user-form'>
-              <label htmlFor="email" className='w-[100%] block mb-[10px]'>Email address</label>
-              <input type="text" placeholder='johndoe@gmail.com' />
-              <label htmlFor="pass" className='block mb-[10px]'>Password</label>
-              <input type="password" placeholder='••••••••' />
-              <button className='forgot-btn'>Forgot password?</button>
-              <button className='submit-btn'>Submit</button>
+          <div className="user-form">
+            <label htmlFor="email" className="w-[100%] block mb-[10px]">
+              Email address
+            </label>
+            <input type="text" placeholder="johndoe@gmail.com" />
+            <label htmlFor="pass" className="block mb-[10px]">
+              Password
+            </label>
+            <input type="password" placeholder="••••••••" />
+            <button className="forgot-btn">Forgot password?</button>
+            <button className="submit-btn">Submit</button>
           </div>
-          <p className='register-btn'>Don’t have an account? <button>Register here</button></p>
+          <p className="register-btn">
+            Don’t have an account? <button>Register here</button>
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
